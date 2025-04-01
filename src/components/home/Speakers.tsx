85d143a1c7e07ba26b8577b12f080f17568ff236
chhaveesh <117168809+chhaveesh@users.noreply.github.com>
2025-04-02 00:36:12 +0530

    changes in photo

--- src/components/home/Speakers.tsx
+++ src/components/home/Speakers.tsx
@@ -19,7 +19,7 @@ const speakers = [
     name: "Prof. Santosh Biswas",
     role: "AI Research Director",
     affiliation: "Stanford University",
-    image: "public/sb.jpg",
+    image: "/sb.jpg",
     bio: "Expert in machine learning applications for healthcare",
     social: {
       twitter: "https://twitter.com",
